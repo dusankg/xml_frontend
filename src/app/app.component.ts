bf1b984c22fce656ce22dce0f6b632939f43d371
dusankg <49291251+dusankg@users.noreply.github.com>
2020-06-03 20:35:30 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit} from '@angular/core';
+import {LoginService} from './login/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'xml-frontend';
+export class AppComponent implements OnInit{
+  title = 'frontend-agent';
+
+  constructor(private  router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['home']);
+  }
+
 }
